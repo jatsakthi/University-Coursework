d4c727f96318e224507811b3578155cc775ef167
Azhaku Sakthi Vel M <jatsakthi@gmail.com>
2017-04-19 21:11:56 -0700

    Update crawl

--- Foodie Friend/res/crawl.py
+++ Foodie Friend/res/crawl.py
@@ -2,20 +2,19 @@ from search import GoogleSearch, SearchError
 import click
 
 # Parameters to call with the script
-@click.command()
-@click.option('--food_name', required=True, help='Name of the Food whose ingredients reqd')
-def main(food_name):
+#@click.command()
+#@click.option('--food_name', required=True, help='Name of the Food whose ingredients reqd')
+def find(food_name):
 	try:
 	  gs = GoogleSearch(food_name)
 	  gs.results_per_page = 10
 	  results = gs.get_results()
+	  lines = []
 	  for res in results:
-	    print res.encode("utf8")
+	    lines.append(res.encode("utf8"))
+	  return lines
 	except SearchError, e:
-
 	  print "Search failed: %s" % e
 
 if __name__ == '__main__':
-    main()
-
-
+    find()
